6a88c4167e0a1d59bb233bd2afd0585974d3920f
Frank Staude <frank@staude.net>
2012-12-11 18:11:02 +0000

    Tested with Wordpress 3.5
    git-svn-id: http://plugins.svn.wordpress.org/disable-title/trunk@637142 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- disable-title.php
+++ disable-title.php
@@ -6,7 +6,7 @@ Description: Disable the title per page/post
 Author: Frank Staude
 Version: 0.1
 Author URI: http://www.staude.net/
-Compatibility: WordPress 3.4.2
+Compatibility: WordPress 3.5
 */
 
 /*  Copyright 2012  Frank Staude  (email : frank@staude.net)
